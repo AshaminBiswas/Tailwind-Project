00287a2d06a272da4604a2743dc37a5543da4147
AshaminBiswas <152035553+AshaminBiswas@users.noreply.github.com>
2024-08-20 07:04:08 +0530

    tailwind first project

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      screens:{
+       " ashamin":"1200px"
+      },
+    },
+  },
+  plugins: [],
+}
+
